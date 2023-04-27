1bc4b2d1b2825b0e884fc80b897f6139cc54e21c
daadaadaah <daadaadaah@gmail.com>
2023-04-27 18:35:55 +0900

    스프링 인터셉터 - 요청 로그

--- src/main/java/com/hcommerce/heecommerce/common/WebConfig.java
+++ src/main/java/com/hcommerce/heecommerce/common/WebConfig.java
@@ -4,9 +4,19 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
 
     @Bean
     public FilterRegistrationBean logFilter() {
