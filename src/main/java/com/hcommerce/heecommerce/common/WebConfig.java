1e0b792c997e0a4b4ac8e383a5fde3100566f04b
daadaadaah <daadaadaah@gmail.com>
2023-04-27 14:45:05 +0900

    서블릿 필터 - 요청 로그

--- src/main/java/com/hcommerce/heecommerce/common/WebConfig.java
+++ src/main/java/com/hcommerce/heecommerce/common/WebConfig.java
@@ -1,16 +1,19 @@
 package com.hcommerce.heecommerce.common;
 
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor())
-                .order(1)
-                .addPathPatterns("/admin/**");
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
 }
