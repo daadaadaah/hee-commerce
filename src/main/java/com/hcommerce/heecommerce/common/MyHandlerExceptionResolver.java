c87195d65f16432c2c954d4449fb494b7817a750
daadaadaah <daadaadaah@gmail.com>
2023-04-30 14:40:47 +0900

    [MyHandlerExceptionResolver 추가] BasicErrorController에 의해 만들어진 구조 -> 내가 커스텀한 JSON 구조로 response 수정

--- src/main/java/com/hcommerce/heecommerce/common/MyHandlerExceptionResolver.java
+++ src/main/java/com/hcommerce/heecommerce/common/MyHandlerExceptionResolver.java
@@ -1,15 +1,22 @@
 package com.hcommerce.heecommerce.common;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
@@ -17,7 +24,17 @@ public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
             if(ex instanceof IllegalArgumentException) {
                 log.info("IllegalArgumentException resolver to 400");
 
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                Map<String, Object> errorResponse = new HashMap<>();
+                errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+                errorResponse.put("message", ex.getMessage());
+
+                String result = objectMapper.writeValueAsString(errorResponse);
+
+                response.setStatus(HttpStatus.BAD_REQUEST.value());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().write(result);
+
                 return new ModelAndView();
             }
 
