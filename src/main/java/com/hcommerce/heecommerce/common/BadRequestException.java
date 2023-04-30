57e7b395bb36e4c1a5cb3646135e039af336a954
daadaadaah <daadaadaah@gmail.com>
2023-04-30 17:43:56 +0900

    [ResponseStatusExceptionResolver] @ResponseStatus을 이용해 HTTP 상태 코드 변경 - message로 reason 사용하기

--- src/main/java/com/hcommerce/heecommerce/common/BadRequestException.java
+++ src/main/java/com/hcommerce/heecommerce/common/BadRequestException.java
@@ -3,6 +3,6 @@ package com.hcommerce.heecommerce.common;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 요류")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
 public class BadRequestException extends RuntimeException {
 }
