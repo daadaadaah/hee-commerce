f9513c1d44d8dcbcf0d28b3b330bcccc6262019f
daadaadaah <daadaadaah@gmail.com>
2023-04-30 13:41:02 +0900

    예외 적용 전

---
+++ src/main/java/com/hcommerce/heecommerce/member/MemberController.java
@@ -0,0 +1,23 @@
+package com.hcommerce.heecommerce.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
--- src/main/java/com/hcommerce/heecommerce/order/OrderController.java
+++ src/main/java/com/hcommerce/heecommerce/order/OrderController.java
@@ -3,7 +3,9 @@ package com.hcommerce.heecommerce.order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
