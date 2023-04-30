488ec860cbac24b345b6d7eb237b1498a5d894b9
daadaadaah <daadaadaah@gmail.com>
2023-04-30 14:22:25 +0900

    [MyHandlerExceptionResolver 추가] BasicErrorController에 의해 만들어진 구조의 response

--- src/main/java/com/hcommerce/heecommerce/common/WebConfig.java
+++ src/main/java/com/hcommerce/heecommerce/common/WebConfig.java
@@ -1,11 +1,19 @@
 package com.hcommerce.heecommerce.common;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
