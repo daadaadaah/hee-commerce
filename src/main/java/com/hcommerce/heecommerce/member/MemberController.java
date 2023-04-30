e546035f776e06fcc3b8d4b791b300aa280b7634
daadaadaah <daadaadaah@gmail.com>
2023-04-30 19:30:19 +0900

    [ResponseStatusExceptionResolver] 조건별 동적 처리 : @ResponseStatus로 불가 -> ResponseStatusException 사용

--- src/main/java/com/hcommerce/heecommerce/member/MemberController.java
+++ src/main/java/com/hcommerce/heecommerce/member/MemberController.java
@@ -4,9 +4,11 @@ import com.hcommerce.heecommerce.common.BadRequestException;
 import com.hcommerce.heecommerce.common.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class MemberController {
@@ -31,6 +33,15 @@ public class MemberController {
     @GetMapping("/api/response-status-ex1")
     public String responseStatusEx1() {
         throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2/{case_id}")
+    public String responseStatusEx2(@PathVariable("case_id") String case_id) {
+        if(case_id.equals("1")) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "찾을 수 없는 요청입니다.", new IllegalArgumentException());
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 요청 입니다.", new IllegalArgumentException());
+        }
     }
 
     @Data
