4529ef982bc2e66fd2c551018552a42e21b699d3
daadaadaah <daadaadaah@gmail.com>
2023-04-30 17:09:06 +0900

    [ResponseStatusExceptionResolver] @ResponseStatus을 이용해 HTTP 상태 코드 변경

--- src/main/java/com/hcommerce/heecommerce/member/MemberController.java
+++ src/main/java/com/hcommerce/heecommerce/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.hcommerce.heecommerce.member;
 
+import com.hcommerce.heecommerce.common.BadRequestException;
 import com.hcommerce.heecommerce.common.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,6 +26,11 @@ public class MemberController {
         }
 
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
     }
 
     @Data
