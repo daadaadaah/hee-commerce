2917e439c2881ed1077920a8429fed7446c1b2f9
daadaadaah <daadaadaah@gmail.com>
2023-04-30 21:57:48 +0900

    [@ExcpetionHandler+@RestControllerAdvice ] @RestControllerAdvice를 이용하여 @ExcpetionHandler와 핸들러 메서드 분리 - 관심사 분리를 통해 개발자가 비즈니스 로직에 좀더 집중할 수 있게 됨

--- src/main/java/com/hcommerce/heecommerce/member/ExceptionHandlerExampleController.java
+++ src/main/java/com/hcommerce/heecommerce/member/ExceptionHandlerExampleController.java
@@ -1,38 +1,16 @@
 package com.hcommerce.heecommerce.member;
 
-import com.hcommerce.heecommerce.common.ErrorResult;
 import com.hcommerce.heecommerce.common.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class ExceptionHandlerExampleController {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandler(IllegalArgumentException e) {
-        log.error("[exceptionHandler] illegalExHandler : ", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
-        log.error("[exceptionHandler] userExHandler : ", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandler(Exception e) {
-        log.error("[exceptionHandler] exHandler : ", e);
-        return new ErrorResult("EX", "내부 오류");
-    }
 
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
