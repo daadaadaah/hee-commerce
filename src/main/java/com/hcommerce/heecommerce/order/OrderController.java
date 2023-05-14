61d8f7580211283aa1f9529ec2c576103acc527a
daadaadaah <daadaadaah@gmail.com>
2023-05-15 02:30:01 +0900

    @RestControllerAdvice 를 사용하지 않을 때의 예외 처리

--- src/main/java/com/hcommerce/heecommerce/order/OrderController.java
+++ src/main/java/com/hcommerce/heecommerce/order/OrderController.java
@@ -1,8 +1,8 @@
 package com.hcommerce.heecommerce.order;
 
-import com.hcommerce.heecommerce.common.dto.CommandSuccessResponseDto;
+import com.hcommerce.heecommerce.common.dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +20,18 @@ public class OrderController {
     }
 
     @PatchMapping("/admin/orders/{orderUuid}/order-receipt-complete")
-    public ResponseEntity<CommandSuccessResponseDto> completeOrderReceipt(@PathVariable("orderUuid") UUID orderUuid) {
+    public ResponseDto completeOrderReceipt(@PathVariable("orderUuid") UUID orderUuid) {
 
-        orderService.completeOrderReceipt(orderUuid);
+        try {
+            orderService.completeOrderReceipt(orderUuid);
+        } catch (OrderOverStockException ex) {
+            return new ResponseDto(HttpStatus.CONFLICT.name(), ex.getMessage());
+        } catch (OrderNotFoundException ex) {
+            return new ResponseDto(HttpStatus.NOT_FOUND.name(), ex.getMessage());
+        } catch (Exception ex) {
+            return new ResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.name(), "내부 서버 오류가 발생했습니다.");
+        }
 
-        return ResponseEntity.ok()
-                .body(new CommandSuccessResponseDto("주문 접수 완료가 처리되었습니다."));
+        return new ResponseDto(HttpStatus.OK.name(), "주문 접수 완료가 처리되었습니다.");
     }
 }
--- src/main/java/com/hcommerce/heecommerce/order/OrderControllerAdvice.java
+++ src/main/java/com/hcommerce/heecommerce/order/OrderControllerAdvice.java
@@ -5,10 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+//@RestControllerAdvice
 public class OrderControllerAdvice {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
