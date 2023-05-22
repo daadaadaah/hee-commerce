7e4114f1586ad3e61db6273bb8f8c30edc705e5e
daadaadaah <daadaadaah@gmail.com>
2023-05-23 00:18:50 +0900

    Stream 실습 (임시 코드임) - https://www.youtube.com/watch?v=Gmwh-tUr_1E&ab_channel=SpringDeveloper

--- src/main/java/com/hcommerce/heecommerce/HeeCommerceApplication.java
+++ src/main/java/com/hcommerce/heecommerce/HeeCommerceApplication.java
@@ -1,5 +1,10 @@
 package com.hcommerce.heecommerce;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisStreamCommands;
+import java.util.Collections;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +12,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HeeCommerceApplication {
 
 	public static void main(String[] args) {
+
+		RedisClient client = RedisClient.create("redis://localhost");
+		StatefulRedisConnection<String, String> connection = client.connect();
+		RedisStreamCommands<String, String> commands = connection.sync();
+
+		Map<String, String> body = Collections.singletonMap("value", "Hello World");
+
+		commands.xadd("my_stream", body);
+
 		SpringApplication.run(HeeCommerceApplication.class, args);
 	}
 }
