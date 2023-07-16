de6150861088d0ebbab084f3473bbaaec41e8dad
daadaadaah <daadaadaah@gmail.com>
2023-07-16 21:31:22 +0900

    temp

---
+++ src/main/java/com/hcommerce/heecommerce/inventory/InventoryEventStatus.java
@@ -0,0 +1,12 @@
+package com.hcommerce.heecommerce.inventory;
+
+public class InventoryEventStatus {
+    /** 재고 감소 경우 **/
+    // 주문
+
+    /** 재고 증가 경우 **/
+    // 주문 실패 : 결제 실패, 토스페이먼튼 승인 유효시간 초과, 토스페이먼트 승인 실패
+    // 주문 취소 : 결제창 닫기, 결제 취소
+
+    // 결제
+}
